f76b40f925aa3cf376a718382961bd0364b7d6a5
cbrss <88015233+cbrss@users.noreply.github.com>
2022-09-08 13:52:30 -0300

    "Jugando con divisores nivel 1" hecho

---
+++ ResolucionesCristian/Jugando_con_divisores/src/divisores/Jugador.java
@@ -0,0 +1,18 @@
+package divisores;
+
+public class Jugador {
+	private int puntuacion;
+
+	public Jugador() {
+
+	}
+
+	public Jugador(int puntuacion) {
+		this.puntuacion = puntuacion;
+	}
+
+	public int getPuntuacion() {
+		return this.puntuacion;
+	}
+
+}
