c2a7ae5c0a0fb996a1e9e1bb27c28edf947dbf84
Pablo Ferreira <pablomartinferre@gmail.com>
2022-09-15 15:54:39 -0300

    implementado quicksort

--- ResolucionesPablo/Ordenamiento/src/algoritmos/OrdenEnteros.java
+++ ResolucionesPablo/Ordenamiento/src/algoritmos/OrdenEnteros.java
@@ -2,19 +2,49 @@ package algoritmos;
 
 public class OrdenEnteros {
 
-    public int[] quicksort(int[] arr) {
-	
+    public static int[] quicksort(int[] arr) {
+	// Wrapper con la implementacion de quicksort qeu devuelve el array ya ordenado
+	// (a pesar de ordenar en el lugar) para permitir concatenacion de funciones
+
 	if(arr.length < 2)
 	    return arr;
-	
-	
-	
-	
+
+	quicksortInterno(arr, 0, arr.length - 1);
+	return arr;
     }
-    
-    private int[] quicksortInterno(int[] arr, int ini, int fin) {
-	
+
+    private static void quicksortInterno(int[] arr, int ini, int fin) {
+	// Algoritmo de quicksort con ultimo elemento como pivot
+
+	if(ini >= fin)
+	    return;
+
+	int pi = particionar(arr, ini, fin);
+	quicksortInterno(arr, ini, pi - 1);
+	quicksortInterno(arr, pi + 1, fin);
+    }
+
+    private static int particionar(int[] arr, int ini, int fin) {
+	// Utilidad de particion para quicksort con ultimo elemento como pivot
+
 	int pivot = arr[fin];
+	int iMenor = ini - 1;
+
+	for(int j = ini; j < fin; j++) {
+	    if(arr[j] < pivot)
+		iMenor++;
+	}
+	intercambiar(arr, iMenor + 1, fin);
+
+	return iMenor + 1;
     }
-    
+
+    private static void intercambiar(int[] arr, int idxA, int idxB) {
+	// Utilidad para intercambiar dos elementos A y B en un array
+
+	int aux = arr[idxA];
+	arr[idxA] = arr[idxB];
+	arr[idxB] = aux;
+    }
+
 }
--- ResolucionesPablo/Ordenamiento/src/test/Pruebas.java
+++ ResolucionesPablo/Ordenamiento/src/test/Pruebas.java
@@ -1,11 +1,42 @@
 package test;
 
+import org.junit.Assert;
 import org.junit.Test;
+import algoritmos.OrdenEnteros;
 
 public class Pruebas {
 
-    @Test
-    public void pruebaQuicksort() {
-	
+    public static void main(String[] args) {
+
+	int arr[] = new int[] { 2, 2, 1, 4, 3, 3, 5 };
+	OrdenEnteros.quicksort(arr);
+
+	for(int i = 0; i < arr.length; i++)
+	    System.out.println(arr[i]);
+
     }
+
+    @Test
+    public void pruebaQuicksortSimple() {
+
+	int arr[] = new int[] { 2, 1, 4, 3, 5 };
+	Assert.assertArrayEquals(new int[] { 1, 2, 3, 4, 5 },
+				OrdenEnteros.quicksort(arr));
+    }
+
+    @Test
+    public void pruebaQuicksortRepetidos() {
+
+	int arr[] = new int[] { 2, 2, 1, 4, 3, 3, 5 };
+	Assert.assertArrayEquals(new int[] { 1, 2, 2, 3, 3, 4, 5 },
+				OrdenEnteros.quicksort(arr));
+    }
+
+    @Test
+    public void pruebaQuicksortVacio() {
+
+	int arr[] = new int[] { };
+	Assert.assertArrayEquals(new int[] { }, OrdenEnteros.quicksort(arr));
+    }
+
 }
