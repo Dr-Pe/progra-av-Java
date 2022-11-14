207cd867236ebe8b0645fda8036f518802c01710
Pablo Ferreira <pablomartinferre@gmail.com>
2022-11-14 18:19:26 -0300

    prolog del 2do parcial

---
+++ ResolucionesPablo/Prolog/parcial2.pl
@@ -0,0 +1,38 @@
+%%% INTENTO DEL EJ 1A DEL PARCIAL 2 
+%   SEGÚN LO QUE RECUERDO DE LA CONSIGNA 
+%   						14/11/22 %%%
+
+gusta(lucas, cafe).
+gusta(hernan, cerveza).
+gusta(yo, cafe).
+
+sirve(gato_negro, cafe).
+sirve(ideas, cafe).
+sirve(ideas, cerveza).
+sirve(encuentros, cerveza).
+
+frecuenta(lucas, ideas).
+frecuenta(hernan, gato_negro).
+frecuenta(yo, encuentros).
+
+
+%%% RESOLUCION %%%
+
+%%% 1 - bares donde lucas tiene cosas que le gusten pero hernan no %%%
+gusta_bar(Per, Bar):-
+    gusta(Per, Beb),
+    sirve(Bar, Beb).
+
+lucasyhernan(Bar):-
+    gusta_bar(lucas, Bar),
+    gusta_bar(hernan, Bar).
+
+lucas_no_hernan(Bar):-
+    gusta_bar(lucas, Bar),
+    \+ lucasyhernan(Bar). % NO DA CONJ VACÍO. CREO QUE EL PARCIAL ESTABA BIEN
+
+%%% 2 - personas que frecuenten bares sin cosas que les gusten %%%
+
+sufridos(Per):-
+    frecuenta(Per, Bar),
+    \+ gusta_bar(Per, Bar).
